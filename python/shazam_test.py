9307f09a2f671dc4e1e2a1a335b06cf596275a81
maurocastermans <mauro.castermans@gmail.com>
2025-01-18 17:16:02 +0100

    23 commit212eqewqhjhwrqrqeqweqweqw

--- python/shazam_test.py
+++ python/shazam_test.py
@@ -11,13 +11,14 @@ class ShazamService:
         self.shazam = Shazam()
 
     async def _recognize_song(self, audio_wav_buffer):
-        return await self.shazam.recognize(audio_wav_buffer.read())
+        return await self.shazam.recognize(audio_wav_buffer)
 
     def identify_song(self, audio_wav_buffer):
         loop = asyncio.new_event_loop()
         try:
             asyncio.set_event_loop(loop)
             result = loop.run_until_complete(self._recognize_song(audio_wav_buffer))
+            logger.info("result is ", result)
             if result and 'track' in result:
                 track = result['track']
                 album_art = track.get('images', {}).get('coverart', 'No cover art available')

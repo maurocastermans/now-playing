bf71ef99d1b0ab53283754374f4a27efed005077
maurocastermans <mauro.castermans@gmail.com>
2025-02-22 12:50:40 +0100

    lots

--- python/logger.py
+++ python/logger.py
@@ -10,20 +10,21 @@ class Logger:
         self._setup_logger()
 
     def _setup_logger(self) -> None:
-        # Set the overall logging level
-        self.logger.setLevel(logging.DEBUG)
+        if not self.logger.hasHandlers():
+            # Set the overall logging level
+            self.logger.setLevel(logging.DEBUG)
 
-        # Stream handler for console logging
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setLevel(logging.DEBUG)
-        stdout_handler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s'))
-        self.logger.addHandler(stdout_handler)
+            # Stream handler for console logging
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            stdout_handler.setLevel(logging.DEBUG)
+            stdout_handler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s'))
+            self.logger.addHandler(stdout_handler)
 
-        # File handler with rotation
-        file_handler = RotatingFileHandler(self.log_file_path, maxBytes=2000, backupCount=3)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s'))
-        self.logger.addHandler(file_handler)
+            # File handler with rotation
+            file_handler = RotatingFileHandler(self.log_file_path, maxBytes=2000, backupCount=3)
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s'))
+            self.logger.addHandler(file_handler)
 
     def get_logger(self) -> logging.Logger:
         return self.logger

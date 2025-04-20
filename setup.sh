b2d7a50fa389b863ea1d21a8d42f8c9a3ac12e18
maurocastermans <mauro.castermans@gmail.com>
2025-04-20 18:43:28 +0200

    Remove cd Downloads in setup script

--- setup.sh
+++ setup.sh
@@ -27,7 +27,6 @@ if python3.9 --version &>/dev/null; then
 else
     echo "==> Installing Python3.9..."
     sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
-    cd ~/Downloads || exit 1
     wget https://www.python.org/ftp/python/3.9.19/Python-3.9.19.tgz
     sudo tar zxf Python-3.9.19.tgz
     cd Python-3.9.19 || exit 1
